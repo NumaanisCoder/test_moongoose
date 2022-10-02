017d8c190b785520d12211c6ae322a4d614ee078
NumaanisCoder <nomannaeem985@gmail.com>
2022-10-02 17:50:04 +0530

    make it better

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,10 +1,1 @@
-mongoose.connect(DB,{
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-.then(()=>{
-    console.log("Connection Successfull");
-})
-.catch(err =>{
-    console.log(err);
-})
+process.env.PORT 
